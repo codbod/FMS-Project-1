4a9e7a556a6f5b1fe5610aac31837613b7e0fce8
Cody Yeung <116022463+codbod@users.noreply.github.com>
2022-10-20 21:31:03 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,26 @@ let bg;
 let y = 0;
 function setup() {
   bg = loadImage('background.jpg');
-  createCanvas(1600, 900);
+  createCanvas(600,400);
+  
+  button = createButton('click me');
+  button.position(50, 100);
+  button.mousePressed(game1);
+  
+  button = createButton('click me2');
+  button.position(50, 169);
+  button.mousePressed(game2);
+  
+  button = createButton('click me3');
+  button.position(50, 250);
+  button.mousePressed(game3);
+}
+
+function game1{
+}
+function game2{
+}
+function game3{
 }
 
 function draw() {
